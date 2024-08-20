3636bd300dd4babf5de4d9d9d6a3051e735cac76
JoshuaKlot <107051800+JoshuaKlot@users.noreply.github.com>
2024-08-20 11:09:24 -0400

    Added lvl clear

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -1,29 +1,56 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class NextLevel : MonoBehaviour
 {
     [SerializeField] private string NextScene;
+    [SerializeField] private AudioSource levelFinish;
+    [SerializeField] private Image FadeToBlack;
+    [SerializeField] private float fadeDuration = 1.0f; // Time it takes to fade to black
+    [SerializeField] private float delayBeforeLoading = 1.0f; // Time to wait after the jingle finishes
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    private bool hasTriggered = false;
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Player"))
+        if (col.CompareTag("Player") && !hasTriggered)
         {
-            SceneManager.LoadScene(NextScene);
+            hasTriggered = true;
+            StartCoroutine(HandleLevelFinish());
         }
+    }
+
+    private IEnumerator HandleLevelFinish()
+    {
+        // Play the jingle
+        if (levelFinish != null)
+        {
+            levelFinish.Play();
+            yield return new WaitForSeconds(levelFinish.clip.length);
+        }
+
+        // Fade to black
+        if (FadeToBlack != null)
+        {
+            float elapsedTime = 0.0f;
+            Color fadeColor = FadeToBlack.color;
+
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                fadeColor.a = Mathf.Clamp01(elapsedTime / fadeDuration);
+                FadeToBlack.color = fadeColor;
+                yield return null;
+            }
+        }
+
+        // Wait for a moment before loading the next scene
+        yield return new WaitForSeconds(delayBeforeLoading);
+
+        // Load the next scene
+        SceneManager.LoadScene(NextScene);
     }
 }
